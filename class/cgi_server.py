19e9af25413ebb2d35cfbd14da4db9531b721e48
Jim Renwick <jimrenwick@gmail.com>
2015-08-22 19:20:42 +0000

    Initial server checkin

---
+++ class/cgi_server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import BaseHTTPServer
+import CGIHTTPServer
+import cgitb; cgitb.enable()  ## This line enables CGI error reporting
+
+PORT = 80
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", PORT)
+handler.cgi_directories = ["/"]
+
+httpd = server(server_address, handler)
+print "serving at port", PORT
+httpd.serve_forever()
+
